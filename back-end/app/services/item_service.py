eabfe3e1f0e579ba48e4b6693ca24f96854a671b
Jasiel <jpsantana@apoioprodesp.sp.gov.br>
2025-10-20 10:20:22 -0300

    ajuste

--- back-end/app/services/item_service.py
+++ back-end/app/services/item_service.py
@@ -1,12 +1,15 @@
 from sqlalchemy.orm import Session
-from app import models, schemas
+from app.models.item import Item
+from app.schemas.item_schema import ItemCreate
 
-def create_item(db: Session, item: schemas.ItemCreate):
-    db_item = models.item.Item(nome=item.nome, descricao=item.descricao)
+
+def create_item(db: Session, item: ItemCreate):
+    db_item = Item(nome=item.nome, descricao=item.descricao)
     db.add(db_item)
     db.commit()
     db.refresh(db_item)
     return db_item
 
+
 def get_items(db: Session):
-    return db.query(models.item.Item).all()
+    return db.query(Item).all()
